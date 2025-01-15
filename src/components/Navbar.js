407a8da666d2ee4557b8e4c3a5fc26cb277afa76
Zwartmit <brandonurbanobuz@gmail.com>
2025-01-15 11:26:56 -0500

    v9.1

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 import { FaHome, FaUser, FaBriefcase } from "react-icons/fa";
